e7ff62b4d87812f60cdcfb0ceba4f2ddd465c73f
siva <mangalisivapullaiah@gmail.com>
2023-07-23 20:57:43 +0530

    ASSIGNMENTPracticeAssignmentOnDSA completed

--- ASSIGNMENTPracticeAssignmentOnDSA/Java 1D Array/Solution.java
+++ ASSIGNMENTPracticeAssignmentOnDSA/Java 1D Array/Solution.java
@@ -1,15 +1,14 @@
+// github.com/RodneyShag
+
 import java.util.*;
 
 public class Solution {
-
     public static void main(String[] args) {
-	   
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-    int a[]=new int[n];
-        for(int i=0;i<n;i++)
-        {
-        a[i]=scan.nextInt();
+        int [] a = new int[n];
+        for (int i = 0 ; i < n; i++) {
+            a[i] = scan.nextInt();
         }
         scan.close();
 
